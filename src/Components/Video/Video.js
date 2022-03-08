d9a9b747f9bc31866c86a545f17dc5195e3a1410
Maria Frentescu <mfrentescu@deloittece.com>
2022-03-08 22:19:24 +0200

    change video background

--- src/Components/Video/Video.js
+++ src/Components/Video/Video.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Video.css";
 import fireBackground from "../../Images/backgroundVid";
-import imgBackground from "../../Images/background";
 
 const Video = () => {
   return (
@@ -9,7 +8,6 @@ const Video = () => {
       <video playsInline muted autoPlay loop src={fireBackground}>
         <source src={fireBackground} type="video/mp4" />
       </video>
-      <img src={imgBackground} alt="img"></img>
     </div>
   );
 };
